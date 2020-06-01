45f583159a21ed9954e91557a203e0c0a475c3d4
Janay Bailey <tjanay87@gmail.com>
2020-06-01 03:45:45 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200601034425) do
+ActiveRecord::Schema.define(version: 20200601034132) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,9 +18,8 @@ ActiveRecord::Schema.define(version: 20200601034425) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "passenger_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taxis", force: :cascade do |t|
